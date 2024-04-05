955333b150329158328fc80a187b8cb2e8cdcc8f
Michael Mbugua <mikembugua254@gmail.com>
2024-04-05 11:26:13 +0300

    Worked with arrow functions

---
+++ index.js
@@ -0,0 +1,19 @@
+// Function Expression
+function divide (a, b){
+    a = 2000;
+    b = 100;
+    return a/b;
+}
+
+//Arrow Function 
+const square = x => {
+    x = 2;
+    return x * x;
+}
+
+//Arrow Function
+const add = (a, b) => {
+    a = 3;
+    b = 4;
+    return a + b;
+} 
